2c186d2b4cd9eddac8e09f5f1309cfb41ee78022
joaokruschewsky <joaokruschewsky@gmail.com>
2023-11-20 15:50:45 -0300

    nest throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 600,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [AppService],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -20,9 +20,10 @@ import { Roles } from 'src/decorators/roles.decorator';
 import { Role } from 'src/enums/role.enum';
 import { RoleGuard } from 'src/guards/role.guard';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Roles(Role.Admin)
-@UseGuards(AuthGuard, RoleGuard)
+@UseGuards(ThrottlerGuard, AuthGuard, RoleGuard)
 @UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
